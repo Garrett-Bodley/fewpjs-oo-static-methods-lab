9ca6e3b15138fee38df380b4f6f36278d32f62fd
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-11 15:49:28 -0500

    commit tree 8e3ab4aeb97ac2bd0c49f62952cd90a77c53649c

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    try{
+      return string.match(/[a-zA-Z0-9\-' ]+/g).join('')
+    }
+    catch(err){
+      return ''
+    }
+  }
+
+  static titleize(string){
+    return string.split(' ').map((word, index) => {
+      if(index === 0){
+        return this.capitalize(word)
+      }else if(!word.match(/\bthe\b|\ba\b|\ban\b|\bbut\b|of\b|\band\b|\bfor\b|\bat\b|\bby\b|\band\b|\bfrom\b/)){
+        return this.capitalize(word)
+      }else{
+        return word
+      }
+    }).join(' ')
+  }
 }
